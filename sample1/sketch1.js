00b33a22462def839f744a120f8dab5800cc39cd
june030211 <june030211@gmail.com>
2025-11-29 17:35:23 +0900

    sketch1.js

--- sample1/sketch1.js
+++ sample1/sketch1.js
@@ -1,2 +1,28 @@
-let cnv1;
-function setup() { cnv1 = createCanvas(600, 400); cnv1.parent("task1"); // ← 여기 추가 background(220); fill(0, 0, 0); rect(0, 0, 600, 400); fill(255); rectMode(CENTER); rect(0, 0, 600, 400); noStroke(); fill(255); ellipse(300, 200, 300, 200); fill(255, 0, 0); ellipse(280, 180, 100, 100); fill(0, 255, 0); ellipse(320, 180, 100, 100); fill(0, 0, 255); ellipse(280, 230, 100, 100); fill(255, 255, 0); ellipse(320, 230, 100, 100); }
+function setup() {
+  let cnv1 = createCanvas(600, 400);
+  cnv1.parent("task1");
+  background(220);
+
+  // 배경 검정색
+  fill(0, 0, 0);
+  rectMode(CORNER);
+  rect(0, 0, 600, 400);
+
+  // 흰색 큰 타원
+  noStroke();
+  fill(255);
+  ellipse(300, 200, 300, 200);
+
+  // 4개의 색 원
+  fill(255, 0, 0); // red
+  ellipse(280, 180, 100, 100);
+
+  fill(0, 255, 0); // green
+  ellipse(320, 180, 100, 100);
+
+  fill(0, 0, 255); // blue
+  ellipse(280, 230, 100, 100);
+
+  fill(255, 255, 0); // yellow
+  ellipse(320, 230, 100, 100);
+}
