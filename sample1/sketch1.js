7ede635e6bebc9560561885602e5405626b5f31a
june030211 <june030211@gmail.com>
2025-11-29 17:38:52 +0900

    sketch1.js

--- sample1/sketch1.js
+++ sample1/sketch1.js
@@ -1,11 +1,10 @@
 function setup() {
   let cnv1 = createCanvas(600, 400);
   cnv1.parent("task1");
-  background(220);
 
-  // 배경 검정색
-  fill(0, 0, 0);
+  // 검정 배경
   rectMode(CORNER);
+  fill(0);
   rect(0, 0, 600, 400);
 
   // 흰색 큰 타원
@@ -14,15 +13,19 @@ function setup() {
   ellipse(300, 200, 300, 200);
 
   // 4개의 색 원
-  fill(255, 0, 0); // red
+  fill(255, 0, 0); 
   ellipse(280, 180, 100, 100);
 
-  fill(0, 255, 0); // green
+  fill(0, 255, 0);
   ellipse(320, 180, 100, 100);
 
-  fill(0, 0, 255); // blue
+  fill(0, 0, 255);
   ellipse(280, 230, 100, 100);
 
-  fill(255, 255, 0); // yellow
+  fill(255, 255, 0);
   ellipse(320, 230, 100, 100);
+}
+
+function draw() {
+  // 과제1은 정적 그림 → draw 비움
 }
