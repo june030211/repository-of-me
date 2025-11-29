dce1167f39b7f387626e3ff5ae174d9627417292
june030211 <june030211@gmail.com>
2025-11-29 18:07:41 +0900

    Update sketch1.js

--- sample1/sketch1.js
+++ sample1/sketch1.js
@@ -1,1 +1,16 @@
-new p5((p) => { p.setup = () => { let cnv = p.createCanvas(600, 400); cnv.parent("task1"); p.rectMode(p.CORNER); p.fill(0); p.rect(0, 0, 600, 400); p.noStroke(); p.fill(255); p.ellipse(300, 200, 300, 200); p.fill(255, 0, 0); p.ellipse(280, 180, 100, 100); p.fill(0, 255, 0); p.ellipse(320, 180, 100, 100); p.fill(0, 0, 255); p.ellipse(280, 230, 100, 100); p.fill(255, 255, 0); p.ellipse(320, 230, 100, 100); }; });
+new p5((p) => { p.setup = () => { 
+  let cnv = p.createCanvas(600, 400); cnv.parent("task1"); 
+  p.rectMode(p.CORNER); 
+  p.fill(0, 0, 0); 
+  p.rect(0, 0, 600, 400);
+  p.noStroke(); 
+  p.fill(255);
+  p.ellipse(300, 200, 300, 200); 
+  p.fill(255, 0, 0); 
+  p.ellipse(280, 180, 100, 100); 
+  p.fill(0, 255, 0);
+  p.ellipse(320, 180, 100, 100); 
+  p.fill(0, 0, 255); 
+  p.ellipse(280, 230, 100, 100); 
+  p.fill(255, 255, 0); 
+  p.ellipse(320, 230, 100, 100); }; });
