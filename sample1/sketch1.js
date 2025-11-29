78eb5ebab1046f8e1fad86797b64e39481272abb
june030211 <june030211@gmail.com>
2025-11-29 18:09:25 +0900

    Update sketch1.js

--- sample1/sketch1.js
+++ sample1/sketch1.js
@@ -1,8 +1,10 @@
 new p5((p) => { p.setup = () => { 
   let cnv = p.createCanvas(600, 400); cnv.parent("task1"); 
-  p.rectMode(p.CORNER); 
   p.fill(0, 0, 0); 
   p.rect(0, 0, 600, 400);
+  p.fill(255, 255, 255)
+  p.rectMode(p.CORNER); 
+  p.rect(0, 0, 600, 400)
   p.noStroke(); 
   p.fill(255);
   p.ellipse(300, 200, 300, 200); 
